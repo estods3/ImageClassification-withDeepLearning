51fd0ee2cd8abdf9a558637fb04c984df9d3c07b
Evan <estods76@gmail.com>
2018-12-01 13:34:08 -0800

    reformatted output

--- src/main.py
+++ src/main.py
@@ -112,12 +112,8 @@ print('] 100%')
 
 dataiter = iter(testloader)
 images, labels = dataiter.next()
-
 outputs = net(images)
-
 _, predicted = torch.max(outputs, 1)
-
-print('Predicted: ', ' '.join('%5s' % classes[predicted[j]] for j in range(4)))
 
 correct = 0
 total = 0
@@ -129,7 +125,8 @@ with torch.no_grad():
         total += labels.size(0)
         correct += (predicted == labels).sum().item()
 
-print('Accuracy of the network on the 10000 test images: %d %%' % (100 * correct / total))
+print("\nResults")
+print('\tAccuracy of the network on the 10000 test images: %d %%' % (100 * correct / total))
 
 class_correct = list(0. for i in range(10))
 class_total = list(0. for i in range(10))
@@ -146,5 +143,5 @@ with torch.no_grad():
 i = 0
 for classType in classesOfInterest:
     ind = coiIndices[i]
-    print('Accuracy of %5s : %2d %%' % (classType, 100 * class_correct[ind] / class_total[ind]))
+    print('\tAccuracy of %5s : %2d %%' % (classType, 100 * class_correct[ind] / class_total[ind]))
     i = i + 1
